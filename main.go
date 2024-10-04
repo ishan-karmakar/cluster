fb5b85ad2e6011cd60437baa9ec6e6ea861b43ed
Ishan Karmakar <ishan.karmakar24@gmail.com>
2024-10-03 21:57:06 -0500

    Got heartbeat UDP connection working

--- main.go
+++ main.go
@@ -5,25 +5,29 @@ import (
 	"net"
 )
 
+const HEARTBEAT_ADDR = "localhost:6801"
+
 func main() {
-	udpAddr, err := net.ResolveUDPAddr("udp", "localhost:6801")
+	addr, err := net.ResolveUDPAddr("udp", HEARTBEAT_ADDR)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	conn, err := net.ListenUDP("udp", udpAddr)
+	conn, err := net.ListenUDP("udp", addr)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	go handleUDP(conn)
-
+	handleHeartbeat(conn)
 }
 
-func handleUDP(conn *net.UDPConn) {
-	n, addr, err := conn.ReadFromUDP(nil)
-	if err != nil {
-		log.Fatalln(err)
+func handleHeartbeat(conn *net.UDPConn) {
+	log.Printf("Heartbeat listening on %s...", HEARTBEAT_ADDR)
+	for {
+		_, addr, err := conn.ReadFromUDP(nil)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("Received heartbeat check from", addr)
 	}
-	log.Println(n, addr)
 }

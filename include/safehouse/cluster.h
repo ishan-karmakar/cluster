0c290a7bae97a9be6305772261f800da814062ba
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-10-03 20:09:35 -0500

    feat: Add template for log entry type

--- include/safehouse/cluster.h
+++ include/safehouse/cluster.h
@@ -7,6 +7,11 @@
 #include <optional>
 #include <vector>
 #include <unordered_map>
+#include <thread>
+#include <random>
+#include <spdlog/spdlog.h>
+#include <unistd.h>
+#include <netdb.h>
 
 namespace safehouse {
 namespace cluster {
@@ -29,9 +34,10 @@ struct RPC {
     size_t term;
 };
 
+template <typename T>
 struct LogEntry {
     size_t term;
-    int value;
+    T value;
 };
 
 struct RequestVoteRPC : RPC {
@@ -48,6 +54,7 @@ struct VoteReceivedRPC : RPC {
     bool voteGranted;
 };
 
+template <typename T>
 struct AppendEntriesRPC : RPC {
     AppendEntriesRPC() : RPC{AppendEntries} {}
 
@@ -55,7 +62,7 @@ struct AppendEntriesRPC : RPC {
     size_t prevLogTerm;
     size_t leaderCommit;
     size_t numEntries;
-    LogEntry entries[0];
+    LogEntry<T> entries[0];
 };
 
 struct AppendEntriesReceivedRPC : RPC {
@@ -65,37 +72,229 @@ struct AppendEntriesReceivedRPC : RPC {
     size_t nextIndex;
 };
 
+template <typename T>
 class Node {
 public:
-    Node(std::initializer_list<std::string> peers);
-    void operator()();
+    Node(std::initializer_list<std::string> peerIps) : ip{get_ip()}, role{Follower} {
+        for (const auto& peer : peerIps) {
+            struct addrinfo hints{}, *res = nullptr;
+            hints.ai_family = AF_INET;
+            hints.ai_socktype = SOCK_DGRAM;
+            if (getaddrinfo(peer.c_str(), nullptr, &hints, &res) == 0 && res) {
+                in_addr_t addr = ((struct sockaddr_in*) res->ai_addr)->sin_addr.s_addr;
+                peers.push_back(addr);
+                freeaddrinfo(res);
+            }
+        }
+        reset_timeout();
+    }
+
+    void operator()() {
+        std::thread listener{&Node::listen, this};
+        std::thread main_thread{[this] {
+            while (true) {
+                if (role == Follower) follower_loop();
+                else if (role == Candidate) candidate_loop();
+                else leader_loop();
+            }
+        }};
+        main_thread.join();
+        listener.join();
+    }
 
 private:
-    void follower_loop();
-    void candidate_loop();
-    void leader_loop();
+    void follower_loop() {
+        spdlog::info("This node is now a follower");
+        while (role == Follower) {
+            std::unique_lock<std::mutex> lock{mtx};
+            if (cv.wait_until(lock, electionDeadline) == std::cv_status::timeout)
+                role = Candidate;
+        }
+    }
 
-    void send_msg(in_addr_t ip, RPC*, size_t);
+    void candidate_loop() {
+        spdlog::info("This node is now a candidate");
+        currentTerm++;
+        votesReceived = 1;
+        votedFor = std::nullopt;
 
-    void listen();
+        RequestVoteRPC msg;
+        for (auto peer : peers)
+            if (peer != ip) send_msg(peer, &msg, sizeof(msg));
+        
+        reset_timeout();
+        while (role == Candidate) {
+            {
+                std::unique_lock<std::mutex> lock{mtx};
+                if (cv.wait_until(lock, electionDeadline) == std::cv_status::timeout) {
+                    spdlog::warn("Election failed, time to start a new election");
+                    break;
+                }
+            }
+            if (votesReceived > peers.size() / 2) {
+                role = Leader;
+                break;
+            }
+        }
+    }
 
-    void reset_timeout();
+    void leader_loop() {
+        spdlog::info("This node is now the leader");
+        for (auto peer : peers) {
+            nextIndex[peer] = log.size();
+            matchIndex[peer] = 0;
+        }
+        while (role == Leader) {
+            for (auto peer : peers) {
+                if (peer == ip) continue;
+                size_t numEntries = log.size() - nextIndex[peer];
+                size_t size = sizeof(AppendEntriesRPC<T>) + sizeof(LogEntry<T>) * numEntries;
+                std::unique_ptr<AppendEntriesRPC<T>> msg{static_cast<AppendEntriesRPC<T>*>(operator new(size))};
+                *msg = {};
+                msg->numEntries = numEntries;
+                for (size_t i = 0; i < numEntries; i++)
+                    msg->entries[i] = log[nextIndex[peer] + i];
+                msg->prevLogIndex = std::nullopt;
+                if (nextIndex[peer]) {
+                    msg->prevLogIndex = nextIndex[peer] - 1;
+                    msg->prevLogTerm = log[msg->prevLogIndex.value()].term;
+                }
+                msg->leaderCommit = commitIndex;
+                send_msg(peer, msg.get(), size);
+            }
+            {
+                std::unique_lock<std::mutex> lock{mtx};
+                if (!cv.wait_for(lock, std::chrono::milliseconds{100}, [this] { return commitIndex == log.size(); }))
+                    spdlog::warn("Leader timed out while waiting for commit confirmation");
+                std::this_thread::sleep_for(std::chrono::milliseconds{100});
+            }
+        }
+    }
 
-    static in_addr_t get_ip();
+    void send_msg(in_addr_t ip, RPC *msg, size_t length) {
+        msg->term = currentTerm;
+        int sock = socket(AF_INET, SOCK_DGRAM, 0);
+        struct sockaddr_in dest = { 0 };
+        dest.sin_port = htons(5000);
+        dest.sin_family = AF_INET;
+        dest.sin_addr.s_addr = ip;
+        sendto(sock, msg, length, 0, reinterpret_cast<struct sockaddr*>(&dest), sizeof(dest));
+        close(sock);
+    }
+
+    void listen() {
+        serverSock = socket(AF_INET, SOCK_DGRAM, 0);
+        sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(5000);
+        addr.sin_addr.s_addr=  INADDR_ANY;
+        bind(serverSock, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+        RPC msg;
+        while (true) {
+            sockaddr_in from;
+            socklen_t fromlen = sizeof(from);
+            if (recvfrom(serverSock, &msg, sizeof(msg), MSG_PEEK, reinterpret_cast<sockaddr*>(&from), &fromlen) == 0) continue;
+            if (msg.term > currentTerm) {
+                currentTerm = msg.term;
+                role = Follower;
+                votedFor = std::nullopt;
+                reset_timeout();
+            }
+
+            if (msg.type == RequestVote) {
+                RequestVoteRPC msg;
+                if (recv(serverSock, &msg, sizeof(msg), 0) > 0) {
+                    if (role == Follower && !votedFor.load().has_value()) {
+                        VoteReceivedRPC response;
+                        if (msg.term < currentTerm)
+                            response.voteGranted = false;
+                        else {
+                            response.voteGranted = true; 
+                            votedFor = from.sin_addr.s_addr;
+                            reset_timeout();
+                        }
+                        send_msg(from.sin_addr.s_addr, &response, sizeof(response));
+                    }
+                }
+            } else if (msg.type == VoteReceived) {
+                VoteReceivedRPC msg;
+                if (recv(serverSock, &msg, sizeof(msg), 0) > 0 && msg.voteGranted)
+                    votesReceived++;
+            } else if (msg.type == AppendEntries) {
+                auto msg = std::make_unique<AppendEntriesRPC<T>>();
+                if (!recv(serverSock, msg.get(), sizeof(AppendEntriesRPC<T>), MSG_PEEK)) break;
+                size_t size = sizeof(AppendEntriesRPC<T>) + msg->numEntries * sizeof(LogEntry<T>);
+                msg = std::unique_ptr<AppendEntriesRPC<T>>{static_cast<AppendEntriesRPC<T>*>(operator new(size))};
+                if (recv(serverSock, msg.get(), size, 0) > 0) {
+                    reset_timeout();
+                    AppendEntriesReceivedRPC response;
+                    response.success = msg->term >= currentTerm && (!msg->prevLogIndex || (msg->prevLogIndex <= log.size() && msg->prevLogTerm == log[msg->prevLogIndex.value()].term));
+                    if (response.success) {
+                        commitIndex = msg->leaderCommit;
+                        if (msg->prevLogIndex)
+                            commitIndex = std::min(commitIndex.load(), msg->prevLogIndex.value() + msg->numEntries);
+                        for (int i = 0; i < msg->numEntries; i++)
+                            log.push_back(msg->entries[i]);
+                    }
+                    response.nextIndex = log.size();
+                    send_msg(from.sin_addr.s_addr, &response, sizeof(response));
+                }
+            } else if (msg.type == AppendEntriesReceived) {
+                AppendEntriesReceivedRPC msg;
+                if (recv(serverSock, &msg, sizeof(msg), 0) > 0) {
+                    nextIndex[from.sin_addr.s_addr] = msg.nextIndex;
+                    matchIndex[from.sin_addr.s_addr] = msg.nextIndex;
+                    for (size_t i = log.size(); i > commitIndex; i--) {
+                        size_t count = 1;
+                        for (auto mi : matchIndex)
+                            if (mi.second >= i) count++;
+                        if (count > peers.size() / 2 && log[i - 1].term == currentTerm) {
+                            commitIndex = i;
+                            break;
+                        }
+                    }
+                    if (!msg.success)
+                        spdlog::warn("Node failed to apply AppendEntries");
+                }
+            }
+            cv.notify_all();
+        }
+    }
+
+    void reset_timeout() {
+        std::mt19937 rng{std::random_device{}()};
+        electionDeadline = std::chrono::steady_clock::now() + std::chrono::milliseconds{std::uniform_int_distribution<size_t>{500, 1000}(rng)};
+    }
+
+    static in_addr_t get_ip() {
+        char hostname[256];
+        if (gethostname(hostname, sizeof(hostname)) == 0) {
+            struct addrinfo hints{}, *res = nullptr;
+            hints.ai_family = AF_INET;
+            hints.ai_socktype = SOCK_DGRAM;
+            if (getaddrinfo(hostname, nullptr, &hints, &res) == 0 && res) {
+                in_addr_t addr = ((struct sockaddr_in*)res->ai_addr)->sin_addr.s_addr;
+                freeaddrinfo(res);
+                return addr;
+            }
+        }
+        spdlog::critical("Could not find own IP address, aborting...");
+        std::exit(1);
+    }
 
     in_addr_t ip;
     std::vector<in_addr_t> peers;
     std::atomic<Role> role;
-    int server_sock;
+    int serverSock;
     std::mutex mtx;
     std::condition_variable cv;
-    std::atomic<size_t> votes_received;
-    std::chrono::steady_clock::time_point election_deadline;
+    std::atomic<size_t> votesReceived;
+    std::chrono::steady_clock::time_point electionDeadline;
     
     // Raft states
     std::atomic<size_t> currentTerm;
     std::atomic<std::optional<in_addr_t>> votedFor;
-    std::vector<LogEntry> log;
+    std::vector<LogEntry<T>> log;
     std::atomic<size_t> commitIndex;
     std::atomic<size_t> lastApplied;
     std::unordered_map<in_addr_t, size_t> nextIndex;
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "safehouse/cluster.h"
 
 int main() {
-    safehouse::cluster::Node{"raft-node-1", "raft-node-2", "raft-node-3"}();
+    safehouse::cluster::Node<int>{"raft-node-1", "raft-node-2", "raft-node-3"}();
     return 0;
 }
